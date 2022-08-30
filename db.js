a160a0b4ecdf16809897f6834e423f70d338c9ae
bkabhi <bkabhijeet9801@gmail.com>
2022-08-30 15:31:06 +0530

    op

---
+++ db.js
@@ -0,0 +1,36 @@
+const items = require("./data/items");
+const tags = require("./data/tags");
+const brands = require("./data/brands");
+const itemTypes = require("./data/itemTypes");
+
+module.exports = {
+  items,
+  tags,
+  brands,
+  itemTypes
+}
+
+
+// const items = require("./data/items");
+
+// module.exports = () => {
+
+//   var tags = items.map((i) => i.tags);
+//   tags = [].concat.apply([], tags);
+//   tags = [...new Set(tags)];
+
+ 
+
+//   var brands = items.map((i) => i.manufacturer);
+//   brands = [...new Set(brands)];
+
+//   var itemTypes = items.map((i) => i.itemType);
+//   itemTypes = [...new Set(itemTypes)];
+
+//   return {
+//     items,
+//     tags,
+//     brands,
+//     itemTypes,
+//   };
+// };
